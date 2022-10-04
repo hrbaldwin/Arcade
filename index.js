c0aee2a1ad581dea331b38e23a3ee2172c83d429
hrbaldwin <haileyrebeccabaldwin@gmail.com>
2022-10-04 11:12:54 -0500

    secondcommit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hiiiii')
+let grid = document.getElementsByClassName('grid');
+let lossMessage = document.getElementsByClassName('lossMessage');
+let playAgain = document.getElementsByClassName('playAgain');
+let scoreDisplay = document.getElementsByClassName('scoreDisplay');
+
+let snake = {
+    body: [ [10, 5], [10, 6], [10, 7], [10,8]],
+    nextDirection: [1, 0]
+}
+
+let gameState = {
+    apple: [11, 8],
+    snake: snake
+}
+function createBoard() {
+    lossMessage.style.display = "none";
+    for (let i = 0; i < 100; i++) {
+        let div = document.createElement('div');
+        grid.appendChild(div);
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.addEventListener('keyup', control);
+    createBoard();
+    startGame();
+    playAgain.addEventListener('click', replay)
+})
+
